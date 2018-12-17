38a218bea2e4f163c96a1bcb89475f767c1373b7
Brock <brock@brocknunn.com>
2018-12-16 23:09:33 -0600

    Build

---
+++ dist/precache-manifest.29c4efbd5aaed89b5ea5ea228430eab5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1cbfe8bd59a854550b8d",
+    "url": "/js/chunk-vendors.2a431ad7.js"
+  },
+  {
+    "revision": "ad3a583b8d80bd526e5b",
+    "url": "/js/chunk-2d22d746.e3607298.js"
+  },
+  {
+    "revision": "4d4acd2e65770ccc9638",
+    "url": "/js/app.18e93d37.js"
+  },
+  {
+    "revision": "52d1a935b42348c7311154e33f14efb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8232a9951ff3b8884dbb22a54a365cdb",
+    "url": "/img/logo.8232a995.png"
+  },
+  {
+    "revision": "4d4acd2e65770ccc9638",
+    "url": "/css/app.0159b29b.css"
+  }
+];
